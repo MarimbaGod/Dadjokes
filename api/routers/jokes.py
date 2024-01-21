3965020b8b30bcedb5e237ad5c5a36cb1af1a9f3
Kyle <bossertkt@gmail.com>
2024-01-21 13:55:08 -0800

    Update jokes.py
    Removed ElevenLabs TTS because ffmpeg issue with the pythonl lib

--- api/routers/jokes.py
+++ api/routers/jokes.py
@@ -5,9 +5,9 @@ import requests
 # import openai
 from elevenlabs import set_api_key, generate, play
 
-# class JokeResponse(BaseModel):
-#     joke: str
-#     audioUrl: str
+class JokeResponse(BaseModel):
+    joke: str
+    audio_base64: str
 
 router = APIRouter()
 
@@ -22,35 +22,39 @@ set_api_key(ELEVENLABS_API_KEY)
 async def get_joke():
     try:
         #API Call to OpenAI
-        # MODEL = "gpt-3.5-turbo"
-        # user_messages = [
-        #     {"role": "system", "content": "You are a helpful assistant."},
-        #     {"role": "user", "content": "Tell me a good Dad Joke."},
-        #     {"role": "user", "content": "Give me a funny joke."},
-        #     {"role": "user", "content": "Share a humorous joke with me."},
-        # ]
-        # joke_response = openai.ChatCompletion.create(
-        #     model=MODEL,
-        #     messages=user_messages,
-        #     temperature=0.9,
-        # )
+        MODEL = "gpt-3.5-turbo"
+        user_messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Tell me a good Dad Joke."},
+            {"role": "user", "content": "Give me a funny joke."},
+            {"role": "user", "content": "Share a humorous joke with me."},
+        ]
+        joke_response = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=user_messages,
+            temperature=0.9,
+        )
         # # cleans up joke text
-        # joke = joke_response.choices[0].message['content']
-        # cleaned_joke = joke.replace("\n", " ")
-        cleaned_joke = "Sure, here's a classic Dad joke for you:  Why don't skeletons fight each other?  Because they don't have the guts!"
+        joke = joke_response.choices[0].message['content']
+        cleaned_joke = joke.replace("\n", " ")
+
+        #### HardCoded joke for testing purposes
+        # cleaned_joke = "Sure, here's a classic Dad joke for you:  Why don't skeletons fight each other?  Because they don't have the guts!"
+
         # ElevenLabs TTS
         # tts_url = "https://api.elevenlabs.io/synthesize"
         # headers = {"Authorization": f"Bearer {ELEVENLABS_API_KEY}"}
         # payload = {"text": joke}
         # audio_response = requests.post(tts_url, headers=headers, json=payload)
 
+        # (removed because of issue with ffmpeg being seen by elevenlabs)
         # audioUrl = audio_response.json()["audioUrl"]
-        audio = generate(
-            text=cleaned_joke,
-            voice="Bella",
-            model="eleven_multilingual_v2"
-        )
-        play(audio)
+        # audio = generate(
+        #     text=cleaned_joke,
+        #     voice="Bella",
+        #     model="eleven_multilingual_v2"
+        # )
+        # play(audio)
         # return JokeResponse(joke=joke, audioUrl=audioUrl)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
